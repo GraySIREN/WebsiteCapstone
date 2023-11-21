4287ff188e255997cb82ac4b5ac3c049cebb9ef2
aaron <agray2195@gmail.com>
2023-11-21 13:42:24 -0600

    Added Cube Game as well as Ashley profile card.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult CubeSmash()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
