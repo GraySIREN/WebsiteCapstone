b92a64021a24e6460810499e1d69de0013719340
aaron <agray2195@gmail.com>
2023-12-20 16:28:29 -0600

    Made some color changes to the home page. Added an embedded SoundCloud media player for one of my songs.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,6 +41,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult React()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
