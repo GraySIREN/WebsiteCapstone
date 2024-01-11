a765f86abf688fbae67c575fefa3eb958816b63d
Aaron <agray2195@gmail.com>
2024-01-11 07:04:07 -0600

    Trying to change site font to Monsterrat. Removed CubeSmash game.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,11 +36,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult CubeSmash()
-    {
-        return View();
-    }
-
     public IActionResult JS()
     {
         return View();
