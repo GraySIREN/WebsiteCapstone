5797d5e56d4b61b69d3181f2abec05c5e3c63a79
Aaron <agray2195@gmail.com>
2024-01-24 12:22:59 -0600

    Removed JS 30 Day View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,10 +36,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult JS()
-    {
-        return View();
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
